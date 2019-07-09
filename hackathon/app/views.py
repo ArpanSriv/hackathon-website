355255fdbe484de8255a7d797b2b280b96d6d12c
Arpan Srivastava <arpansri98@gmail.com>
2019-07-10 02:14:58 +0530

    Init template and static folders

--- hackathon/app/views.py
+++ hackathon/app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("You are on the right way.")
+    return render(request, 'app/landing.html')

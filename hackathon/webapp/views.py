2e02688995ac0c09fea684f183810b9965d2dfbc
Arpan Srivastava <arpansri98@gmail.com>
2019-07-15 08:50:22 +0530

    Form done without upload

--- hackathon/webapp/views.py
+++ hackathon/webapp/views.py
@@ -116,14 +116,13 @@ def registration_individual(request):
                 'message': 'Data found sufficient.'
             }
 
-            reg_no = 'xxxxx'
+            reg_no = ''  # TODO
 
             print("Uploading to sheets...")
 
             # Upload the Data on Spreadsheet and Firebase :p
-            update_google_sheets(INDIVIDUAL, reg_no, json_data['teamName'], json_data['memberDetails'])
-
-            print("Updated Sheets.")
+            update_google_sheets(INDIVIDUAL, reg_no, json_data)
+            upload_on_firebase(INDIVIDUAL, json_data)
 
             return HttpResponse(json.dumps(resp), content_type='application/json')
 
@@ -133,9 +132,76 @@ def registration_individual(request):
 
 def registration_startup(request):
     if request.method == 'POST':
-        received_json_data = json.loads(request.body.decode("utf-8"))
+        json_data = json.loads(request.body.decode("utf-8"))
 
-        print("Recieved::: " + received_json_data)
+        resp = {
+            'correct': '0',
+            'message': 'Internal Server Error.'
+        }
+
+        # Startup Name Empty
+        if 'startupName' in json_data:
+            if json_data['startupName'] == '':
+                resp['message'] = 'A valid Startup Name is required.'
+                return HttpResponseBadRequest(json.dumps(resp), content_type='application/json')
+
+        # Team Email Empty
+        if 'startupEmail' in json_data:
+            if json_data['startupEmail'] == '':
+                resp['message'] = 'A valid Startup Email is required.'
+                return HttpResponseBadRequest(json.dumps(resp), content_type='application/json')
+
+        # Team DOR Empty
+        if 'startupDOR' in json_data:
+            if json_data['startupDOR'] == '':
+                resp['message'] = 'A valid Startup Email is required.'
+                return HttpResponseBadRequest(json.dumps(resp), content_type='application/json')
+
+        # Team Domain Empty
+        if 'startupDomain' in json_data:
+            if json_data['startupDomain'] == '':
+                resp['message'] = 'A valid Startup Technology Domain is required.'
+                return HttpResponseBadRequest(json.dumps(resp), content_type='application/json')
+
+        # Team Domain Empty
+        if json_data['startupDesc'] == '':
+            resp['message'] = 'A valid Startup Description is required.'
+            return HttpResponseBadRequest(json.dumps(resp), content_type='application/json')
+
+        # Team Members Validation
+        if 'member1' not in json_data['memberDetails'] and 'member2' not in json_data['memberDetails']:
+            print('member 1 and member 2 not found. -> Startup')
+
+            resp = {
+                'correct': '0',
+                'message': 'Member 1 and Member 2 are required.'
+            }
+
+            return HttpResponseBadRequest(json.dumps(resp), content_type='application/json')
+
+        elif 'member1' in json_data['memberDetails'] and 'member2' in json_data['memberDetails']:
+            if json_data['memberDetails']['member1']['firstName'] == '' or json_data['memberDetails']['member2']['firstName'] == '':
+                print('member 1 and member 2 found empty -> Startup')
+
+                resp['message'] = 'Members found but empty. Are you sure you entered all details correctly?'
+
+                return HttpResponseBadRequest(json.dumps(resp), content_type='application/json')
+
+            # Found members 1 and 2 and also found some data there.
+            resp = {
+                'correct': '1',
+                'message': 'Data found sufficient.'
+            }
+
+            reg_no = ''  # TODO
+
+            print("Uploading to sheets...")
+
+            # Upload the Data on Spreadsheet and Firebase :p
+            update_google_sheets(STARTUP, reg_no, json_data)
+            upload_on_firebase(STARTUP, json_data)
+
+            return HttpResponse(json.dumps(resp), content_type='application/json')
 
     return render(request, 'webapp/startupregistration.html')
 

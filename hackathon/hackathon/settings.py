3999774ff1b3eca85509666a4ec1c035897ad186
Arpan Srivastava (Ubuntu) <arpansri98@gmail.com>
2019-08-06 19:17:07 +0530

    Updated settings.ini location and DEBUG = False for staging

--- hackathon/hackathon/settings.py
+++ hackathon/hackathon/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0ngtf5br@lwqjxq0eo9c9^qdvo6plsj+5j3b@&4dr5#w$vd^=t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True 
 
 ALLOWED_HOSTS = ['aihackathon.in', 'www.aihackathon.in']
 
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = ['aihackathon.in', 'www.aihackathon.in']
 from configparser import RawConfigParser
 
 config = RawConfigParser()
-config.read('/home/ubuntu/config/settings.ini')
+config.read('/etc/config/hackathon-settings.ini')
 
 environ = config.get('django', 'environment')
 
 if environ == 'staging':
     ALLOWED_HOSTS.append("*")
+    global DEBUG
+    DEBUG = False
 
 # Application definition
 
